495266dae87e1c367f94f7c3763d0477c6460aa9
OsmanOSA <saidaliosman925@gmail.com>
2026-01-05 18:02:30 +0100

    Update setup.py

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import find_packages, setup
+from typing import List, Literal
+
+def get_requirements() -> List[str]:
+    
+    requirement_lst: List[str] = []
+
+    try: 
+
+        with open("requirements.txt", "r") as file:
+            lines = file.readlines()
+
+            for line in lines:
+                requirement = line.strip()
+
+                if requirement and requirement!="-e .":
+                    requirement_lst.append(requirement)
+
+    except FileNotFoundError:
+        print("Fichier requirements.txt non trouvé")
+
+    return requirement_lst
+
+
+setup(
+    name="Customers churn prediction analysis", 
+    version="0.0.1", 
+    author="Osman SAID ALI",
+    author_email="saidaliosman925@gmai.com",
+    packages=find_packages(),
+    install_requires=get_requirements()
+)
